704475ce42b03a7b5fb2f95f5c5eb6e98bde4a8b
gabschlam <gabschlam@gmail.com>
2020-08-24 20:38:01 -0500

    Componente NavBar agregado

--- clase4/src/App.js
+++ clase4/src/App.js
@@ -1,24 +1,14 @@
 import React from "react";
 import Home from "./Pages/Home";
 import User from "./Pages/User";
+import NavBar from "./Components/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
 
 export default function App() {
   return (
     <Router>
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="/">Navbar</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="/home">Home</Nav.Link>
-          <Nav.Link href="/user">User</Nav.Link>
-        </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-info">Search</Button>
-        </Form>
-      </Navbar>
+      <NavBar />
       <Switch>
         <Route path="/home">
           <Home />
